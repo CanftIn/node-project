88a2fa433b42e2e3b5a8653c08124d0cbec28b2c
CanftIn <wwc7033@gmail.com>
2020-02-24 14:55:16 +0800

    eliminate github security alerts

---
+++ vue_learn/app.vue
@@ -0,0 +1,18 @@
+<!-- 指定vue管理内容区域，需要通过vue展示的内容都要放到找个元素中  通常我们也把它叫做边界 数据只在边界内部解析-->
+<div id="app">{{ msg }}</div>
+
+<!-- 引入 vue.js -->
+<script src="vue.js"></script>
+
+<!-- 使用 vue -->
+<script>
+  var vm = new Vue({
+    // el：提供一个在页面上已存在的 DOM 元素作为 Vue 实例的挂载目标
+    el: '#app',
+    // Vue 实例的数据对象，用于给 View 提供数据
+    data: {
+      msg: 'Hello Vue'
+    }
+  })
+  console.log(vm.$data.msg)
+</script>
